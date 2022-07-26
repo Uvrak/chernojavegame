bc077e61418c7f743dc140af2d5900b389ee5841
Uvrak <danielhorn101@gmail.com>
2022-07-26 16:07:58 +0200

    Episode 14 - FPS Counter

--- Rain/src/com/thecherno/rain/Game.java
+++ Rain/src/com/thecherno/rain/Game.java
@@ -17,6 +17,7 @@ public class Game extends Canvas implements Runnable{
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
+	public static String title = "Rain";
 	
 	private Thread thread;
 	private JFrame frame;
@@ -53,19 +54,34 @@ public class Game extends Canvas implements Runnable{
 	@Override
 	public void run() {
 		long lastTime = System.nanoTime();
+		long timer = System.currentTimeMillis();
 		final double ns = 1000000000.0 / 60.0;
 		double delta = 0;
+		int frames = 0;
+		int updates = 0;
 		
 		while(running) {
 			long now = System.nanoTime();
 			//System.out.println(now - lastTime);
 			delta += (now - lastTime) / ns;
+			lastTime = now;
 			while(delta >=1) {
 				update();
-				render();
+				updates++;
 				delta--;
 			}
-			lastTime = now;
+			
+			render();
+			frames++;
+			
+			if (System.currentTimeMillis() - timer >  1000) {
+				timer += 1000;
+				System.out.println(updates + " ups, " + frames + " fps");
+				frame.setTitle(title + "  |  " + updates + " ups, " + frames + " fps");
+				updates = 0;
+				frames = 0;
+			}
+			
 		}
 	}
 	
@@ -81,12 +97,7 @@ public class Game extends Canvas implements Runnable{
 		}
 		
 		screen.clear();
-		/*try {
-			Thread.sleep(10);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		*/
+		
 		screen.render();
 		
 		for(int i = 0; i < pixels.length; i++) {
@@ -101,7 +112,7 @@ public class Game extends Canvas implements Runnable{
 	public static void main(String[] args) {
 		Game game = new Game();
 		game.frame.setResizable(false);
-		game.frame.setTitle("Rain");
+		game.frame.setTitle(game.title);
 		game.frame.add(game);
 		game.frame.pack();
 		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);

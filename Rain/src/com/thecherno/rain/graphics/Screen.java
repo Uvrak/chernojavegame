c9397b805748384a2a4e2139a6d083026d00533a
Uvrak <danielhorn101@gmail.com>
2022-07-26 07:02:10 +0200

    Episode 8 - The Screen Class

---
+++ Rain/src/com/thecherno/rain/graphics/Screen.java
@@ -0,0 +1,16 @@
+package com.thecherno.rain.graphics;
+
+public class Screen {
+
+	private int width;
+	private int height;
+	
+	public int[] pixels;
+	
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		
+		pixels = new int[width * height];
+	}
+}
